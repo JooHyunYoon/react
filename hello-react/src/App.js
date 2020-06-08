704603455350705f2701876473c09a3b529bcd6b
JooHyunYoon <juse22@naver.com>
2020-06-08 23:04:24 +0900

    200608

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,42 +1,30 @@
-import React, { Component } from "react";
-// import MyComponent from './MyComponent';
-// import Counter from './Counter';
-// import Say from './Say';
-// import EventPractice from './EventPractice'
-// import ValidationSample from './ValidationSample'
-// import ScrollBox from './ScrollBox';
-import IterationSample from './IterationSample';
+import React, { Component } from 'react';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 
-// const App = () => {
-  // return <MyComponent name="윤주현"/>;
-  // return <MyComponent />;
-  // return (
-  //   <MyComponent name="React" favoriteNumber={1}>
-  //     리액트
-  //   </MyComponent>
-  // );
-  // return <Counter />;
-  // return <Say />;
-  // return <EventPractice />; 
-// }
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <ScrollBox ref={(ref) => this.scrollBox=ref}/>
-//         <button onClick={() => this.scrollBox.ScrollToBottom()}>
-//           맨 밑으로
-//         </button>
-//       </div>
-//     );
-//   }
-// }
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+
   render() {
     return (
-      <IterationSample />
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
     );
   }
 }
