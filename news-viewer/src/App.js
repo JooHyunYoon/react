312237ec7bec3b61a5ad8eae4f4d7624d9eff43a
JooHyunYoon <juse22@naver.com>
2020-06-18 23:00:45 +0900

    20200618

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,31 +1,9 @@
-import React, { useState } from 'react';
-// import axios from 'axios';
-import NewsList from './components/NewsList';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
-// const App = () => {
-//   const [data, setData] = useState(null);
-//   const onClick = async () => {
-//     try {
-//       const response = await axios.get(
-//         'https://newsapi.org/v2/top-headlines?country=kr&apiKey=23c440e8d5114003b1332ba84769f1ea'
-//       )
-//       setData(response.data);
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
-
-//   return(
-//     <div>
-//       <div>
-//         <butoon onClick={onClick}>불러오기</butoon>
-//       </div>
-//       {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-//     </div>
-//   )
-// }
 const App = () => {
-  return <NewsList />;
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
