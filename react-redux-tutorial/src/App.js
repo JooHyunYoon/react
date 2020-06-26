c73c99a8e7cfe4de4a2ce080df60add34379950d
JooHyunYoon <juse22@naver.com>
2020-06-26 23:45:44 +0900

    20200626

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Counter from './Counter';
-import Todos from './Todo';
+import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
 
 function App() {
   return (
     <div>
-      <Counter number={0} />
+      <CounterContainer />
       <hr />
-      <Todos />
+      <TodosContainer />
     </div>
   );
 }
